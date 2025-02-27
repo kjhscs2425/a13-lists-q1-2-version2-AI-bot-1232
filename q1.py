9ef81878971b164582673d55105ac242cf99c902
AI-bot-1232 <nries2026@kehillahstudent.org>
2025-02-27 10:43:12 -0800

    finished

--- q1.py
+++ q1.py
@@ -5,11 +5,15 @@
 # Assign this list to the variable my_list. Then print my_list
 
 #YOUR CODE HERE
-
+my_list = ["9","7","cat",False]
+print(my_list) 
 # b) Append 3.14 and 7 to the list. Then print my_list
+my_list.append(3.14)
+my_list.append(7)
 
 #YOUR CODE HERE
 
 # c) Find and print the index of "cat"
 
 #YOUR CODE HERE
+print(my_list.index("cat"))
